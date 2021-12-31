e529788a81a9eabe777fe2a14b2e56544ad097ad
Kevin Do <kevind0501@gmail.com>
2021-12-31 12:41:05 -0800

    set up App.js routes and added framework of views

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import { NavLink, Route, Switch } from 'react-router-dom';
+import Home from './views/Home/Home';
+import Player from './views/Player/Player';
+import Players from './views/Players/Players';
+import Team from './views/Team/Team';
+import Teams from './views/Teams/Teams';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Kickball League</h1>
+      <BrowserRouter>
+        <header>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/teams">Teams</NavLink>
+          <NavLink to="/players">Players</NavLink>
+        </header>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="teams" component={Teams} />
+          <Route path="teams/:id" component={Team} />
+          <Route path="players" component={Players} />
+          <Route path="players/:id" component={Player} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
