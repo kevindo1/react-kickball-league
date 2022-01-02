52f7f8a232ca442f94151db715f3938833bd7f19
Kevin Do <kevind0501@gmail.com>
2022-01-01 22:59:07 -0800

    working player page that displays their name and position

--- src/components/TeamDetails.js
+++ src/components/TeamDetails.js
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 
 export default function TeamDetails({ team }) {
   return (
-    <section>
-      <h3>Players</h3>
+    <div>
+      <h3>Team Details</h3>
       {team.players.map((player) => (
         <Link key={player.id} to={`/players/${player.id}`}>
           {player.name}
         </Link>
       ))}
-    </section>
+    </div>
   );
 }
--- src/views/Player/Player.js
+++ src/views/Player/Player.js
@@ -1,5 +1,22 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { fetchPlayersById } from '../../services/players';
+import PlayerDetails from '../../components/PlayerDetails';
 
-export default function Player() {
-  return <div></div>;
+export default function Player(props) {
+  const [player, setPlayer] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchPlayersById(props.match.params.id);
+      setPlayer(data);
+    };
+    fetchData();
+  }, [props.match.params.id]);
+
+  return (
+    <div>
+      <PlayerDetails player={player} />
+    </div>
+  );
 }
