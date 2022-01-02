00c0f388b43bff011a9b5ece64f48f4f0ba56c39
Kevin Do <kevind0501@gmail.com>
2022-01-02 12:51:59 -0800

    got team.js to work by adding loading and setloading, shout out to TA mira

--- src/components/TeamDetails.js
+++ src/components/TeamDetails.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 export default function TeamDetails({ team }) {
   return (
     <div>
+      <h3>Team Details</h3>
       {team.players.map((player) => (
         <Link key={player.id} to={`/players/${player.id}`}>
           {player.name}
--- src/views/Team/Team.js
+++ src/views/Team/Team.js
@@ -5,15 +5,21 @@ import TeamDetails from '../../components/TeamDetails';
 
 export default function Team(props) {
   const [team, setTeam] = useState([]);
+  const [loading, setLoading] = useState(true);
   const id = props.match.params.id;
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetchTeamsById(id);
       setTeam(data);
+      setLoading(false);
     };
     fetchData();
   }, [id]);
+
+  if (loading) {
+    return <h3>loading</h3>;
+  }
 
   return (
     <div>
